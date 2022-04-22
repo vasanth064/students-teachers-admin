8a0b0b40d8291da1876d8927168b9d4e25278195
vasanth064 <vasanthvdev@gmail.com>
2022-04-22 10:49:37 +0530

    added admin emails

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../Context/AuthContext';
 
+const AdminEmails = [
+  'vasanthnaveen2011@gmail.com',
+  'vathsansri597@gmail.com',
+  'rajkumardce19dx18@gmail.com',
+  'ashokku51202@gmail.com',
+];
 export const PrivateRoute = ({ children }) => {
   const { currentUser, logOut } = useAuth();
-  return currentUser ? children : logOut() && <Navigate to='/login' />;
+  const [adminEmails] = useState(AdminEmails);
+  return currentUser && adminEmails.includes(currentUser.email)
+    ? children
+    : logOut() && <Navigate to='/login' />;
 };
