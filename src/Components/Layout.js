2567f315ef4a01a2fd911757f387c08ae212c3d6
vasanth064 <vasanthvdev@gmail.com>
2022-05-06 15:56:39 +0530

    added sidebar

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -6,30 +6,39 @@ const Layout = ({ children }) => {
   const { currentUser, logOut } = useAuth();
   return (
     <div className='App'>
+      <aside className='sidebarContainer'>
+        <ul className='sidebar'>
+          <img
+            src='https://www.psgpolytech.ac.in/img/smalllogo.png'
+            alt='clgLogo'
+            style={{
+              width: '11rem',
+              height: 'auto',
+            }}
+          />
+          {routes.map((route, index) =>
+            route.name !== 'Login' ? (
+              <li key={index} className='sidebarItem'>
+                <Link to={route.path}>{route.name}</Link>
+              </li>
+            ) : null
+          )}
+        </ul>
+      </aside>
       <header>
         <nav>
           <h1>ADMIN DASHBOARD</h1>
           {currentUser ? (
             <ul>
-              {routes.map((route, index) =>
-                route.name !== 'Login' ? (
-                  <li key={index}>
-                    <Link to={route.path}>{route.name}</Link>
-                  </li>
-                ) : currentUser ? (
-                  <li key={index}>
-                    <button
-                      onClick={() => logOut()}
-                      style={{ display: 'inline' }}>
-                      Logout
-                    </button>
-                  </li>
-                ) : (
-                  <li key={index}>
-                    <Link to={route.path}>{route.name}</Link>
-                  </li>
-                )
-              )}
+              {currentUser ? (
+                <li>
+                  <button
+                    onClick={() => logOut()}
+                    style={{ display: 'inline' }}>
+                    Logout
+                  </button>
+                </li>
+              ) : null}
             </ul>
           ) : null}
         </nav>
