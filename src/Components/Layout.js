92a57bedf8d63aff2de0c400e09e6b131f7cb69b
vasanth064 <vasanthvdev@gmail.com>
2022-05-09 03:20:02 +0530

    removed navigation in login

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -6,25 +6,28 @@ const Layout = ({ children }) => {
   const { currentUser, logOut } = useAuth();
   return (
     <div className='App'>
-      <aside className='sidebarContainer'>
-        <ul className='sidebar'>
-          <img
-            src='https://www.psgpolytech.ac.in/img/smalllogo.png'
-            alt='clgLogo'
-            style={{
-              width: '11rem',
-              height: 'auto',
-            }}
-          />
-          {routes.map((route, index) =>
-            route.name !== 'Login' ? (
-              <li key={index} className='sidebarItem'>
-                <Link to={route.path}>{route.name}</Link>
-              </li>
-            ) : null
-          )}
-        </ul>
-      </aside>
+      {currentUser && (
+        <aside className='sidebarContainer'>
+          <ul className='sidebar'>
+            <img
+              src='https://www.psgpolytech.ac.in/img/smalllogo.png'
+              alt='clgLogo'
+              style={{
+                width: '11rem',
+                height: 'auto',
+              }}
+            />
+            {routes.map((route, index) =>
+              route.name !== 'Login' ? (
+                <li key={index} className='sidebarItem'>
+                  <Link to={route.path}>{route.name}</Link>
+                </li>
+              ) : null
+            )}
+          </ul>
+        </aside>
+      )}
+
       <header>
         <nav>
           <h1>ADMIN DASHBOARD</h1>
